25453737a4c8242aabcf6db8e678c539bdb78798
TrustyPatches <fpendlebury@gmail.com>
2015-03-31 22:42:20 +0100

    Include initial bower components and grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+modules.export = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    concat: {
+
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  //grunt.registerTask('default', ['concat']);
+
+};
